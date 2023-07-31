bad888d17b43f4b769a4510b91341417b720f938
Miracle Jude <judemiraco0@gmail.com>
2023-07-31 19:13:45 +0100

    update general data

--- src/components/general-data.ts
+++ src/components/general-data.ts
@@ -13,13 +13,12 @@
 
 type Subject = {
     name?: string;
-  };
-  
-  type Schedule = {
-    [key: string]: Subject[][];
-  };
-  
- export const schedule: Schedule = {
+};
+type Schedule = {
+  [key: string]: Subject[][];
+};
+
+  export const schedule: Schedule = {
     Monday: [
         [
           { name: 'GES 100.1 (TETFUND 7 in 1)' },
@@ -27,197 +26,194 @@ type Subject = {
           { name: 'CSC483.1(MBS22)' },
         ],
         [
-          { name: 'Subject 4' },
-          { name: 'Subject 5' },
-          { name: 'Subject 6' },
+          { name: 'GES100.1 (TETFUND 7 in1)' },
+          { name: 'CSC 394.1(CSC HALL2)' },
+          { name: 'CSC483.1(MBS22)' },
         ],
         [
-          { name: 'Subject 7' },
-          { name: 'Subject 8' },
+          { name: 'MTH110.1(CHALL,MBA1,2)' },
+          { name: 'CSC 394.1(CSC HALL2)' },
         ],
         [
-          { name: 'Subject 9' },
+          { name: 'MTH110.1(FOS,CHALL,MBA1,2)' },
         ],
         [
-          { name: 'Subject 10' },
-          { name: 'Subject 11' },
+          { name: 'STA 260.1 (MBA 2, MTH HALL)' },
+          { name: 'CSC 397.1 (MBS 22)' },
         ],
         [
-          { name: 'Subject 12' },
+          { name: 'CSC486.1(CSC HALL 2)' },
+          { name: 'CSC 397.1 (MBS 22)' },
+          { name: 'STA 260.1 (MBA 2, MTH HALL)' },
         ],
         [
-          { name: 'Subject 13' },
-          { name: 'Subject 14' },
-          { name: 'Subject 15' },
+          { name: 'CSC486.1(CSC HALL 2)' },
+          { name: 'STA 370.1 (MBA 2)' },
         ],
         [
-          { name: 'Subject 16' },
-          { name: 'Subject 17' },
-          { name: 'Subject 18' },
+          { name: 'STA 370.1 (MBA 2)' },
+          { name: 'CSC 281.1 (CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 19' },
-          { name: 'Subject 20' },
+          { name: 'PHY101.1(FoS,CHALL,MBA1,2)' },
+          { name: 'MTH 270.1 (CSC HALL 2, MTH HALL)' },
+        ],
+        [
+          { name: 'CSC 396.1 (MBS 22)' },
+          { name: 'MTH120.1(CHALL,MBA1,2)' },
         ],
       ],
     Tuesday: [
         [
           { name: 'CSC480.1(CSC HALL 2)' },
-          { name: '' },
-          { name: '' },
         ],
         [
-          { name: 'Subject 4' },
-          { name: 'Subject 5' },
-          { name: 'Subject 6' },
+          { name: 'GES100.1 (TETFUND 7 in1)' },
+          { name: 'CSC482.1(MBS22)' },
+          { name: 'CSC 395.1 (CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 7' },
-          { name: 'Subject 8' },
+          { name: 'CSC482.1(MBS22)' },
+          { name: 'CSC283.1(CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 9' },
+          { name: 'CSC283.1(CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 10' },
-          { name: 'Subject 11' },
+          { name: 'CSC 280.1 (MBA 2)' },
+          { name: 'FSB 101.1(FoS,CHALL,MBA1,2)' },
         ],
         [
-          { name: 'Subject 12' },
+          { name: 'FSB 101.1(FoS,CHALL,MBA1,2)' },
+          { name: 'CSC 280.1 (MBA 2)' },
+          { name: 'CSC483.1(CSC HALL2)' },
         ],
         [
-          { name: 'Subject 13' },
-          { name: 'Subject 14' },
-          { name: 'Subject 15' },
+          { name: 'PHY 101.1 (FoS,CHALL,MBA1,2)' },
+          { name: 'CSC 288.1 (CSC 2)' },
+          { name: 'GES 300.1 (CBN HALL)' },
         ],
         [
-          { name: 'Subject 16' },
-          { name: 'Subject 17' },
-          { name: 'Subject 18' },
+          { name: 'fGES 300.1 (CBN HALL)' },
+          { name: 'PHY 101.1 (FoS,CHALL,MBA1,2)' },
         ],
         [
-          { name: 'Subject 19' },
-          { name: 'Subject 20' },
+          { name: 'CHM130.1(FoS,CHALL,MBA1,2)' },
+          { name: 'CSC 284.1 (CSC HALL2)' },
+        ],
+        [
+          { name: 'CHM130.1(FoS,CHALL,MBA1,2)' },
+          { name: 'CSC 284.1 (CSC HALL2)' },
         ],
       ],
     Wednesday: [
         [
           { name: 'MTH 120.1(FoS,CHALL,MBA1,2)' },
-          { name: '' },
-          { name: '' },
         ],
         [
-          { name: 'Subject 4' },
-          { name: 'Subject 5' },
-          { name: 'Subject 6' },
+          { name: 'MTH 120.1(FoS,CHALL,MBA1,2)' },
         ],
         [
-          { name: 'Subject 7' },
-          { name: 'Subject 8' },
+          { name: 'CSC 382.1 (CSC HALL2)' },
         ],
         [
-          { name: 'Subject 9' },
+          { name: 'FSB 101.1 (FoS,CHALL,MBA1,2)' },
+          { name: 'CSC 382.1 (CSC HALL2)' },
         ],
         [
-          { name: 'Subject 10' },
-          { name: 'Subject 11' },
+          { name: 'CSC498.1(CSC HALL2)' },
         ],
         [
-          { name: 'Subject 12' },
+          { name: 'CSC480.1(CSC HALL2)' },
         ],
         [
-          { name: 'Subject 13' },
-          { name: 'Subject 14' },
-          { name: 'Subject 15' },
+          { name: 'GES 100.1 (CBN Hall)' },
+          { name: 'MTH 210.1 (MBA1,2)' },
+          { name: 'CSC480.1(CSC HALL2)' },
+          { name: 'CSC 280.1 (MBA 3)' },
         ],
         [
-          { name: 'Subject 16' },
-          { name: 'Subject 17' },
-          { name: 'Subject 18' },
+          { name: 'MTH 210.1 (MBA1,2)' },
+          { name: 'GES 400.1(MBS 22)' },
         ],
         [
-          { name: 'Subject 19' },
-          { name: 'Subject 20' },
+          { name: 'SPORTS' },
+        ],
+        [
+          { name: 'SPORTS' },
         ],
       ],
     Thursday: [
         [
           { name: 'CSC 284.1 (CSC HAll 2)' },
           { name: 'MTH110.1(FOS,CHALL,MBA1)' },
-          { name: '' },
         ],
         [
-          { name: 'Subject 4' },
-          { name: 'Subject 5' },
-          { name: 'Subject 6' },
+          { name: 'CSC180.1(CSC Hall 2)' },
+          { name: 'CSC 395.1 (MBA 4)' },
         ],
         [
-          { name: 'Subject 7' },
-          { name: 'Subject 8' },
+          { name: 'CSC481.1(MBS22)' },
+          { name: 'CSC 396.1 (CSC HALL2)' },
         ],
         [
-          { name: 'Subject 9' },
+          { name: 'CSC 396.1 (CSC HALL2)' },
         ],
         [
-          { name: 'Subject 10' },
-          { name: 'Subject 11' },
+          { name: 'MTH 270.1 (CSC HALL 2, MTH HALL)' },
         ],
         [
-          { name: 'Subject 12' },
+          { name: 'MTH 270.1 (CSC HALL 2, MTH HALL)' },
         ],
         [
-          { name: 'Subject 13' },
-          { name: 'Subject 14' },
-          { name: 'Subject 15' },
+          { name: 'CSC 397.1 (CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 16' },
-          { name: 'Subject 17' },
-          { name: 'Subject 18' },
+          { name: 'PHY101.1(FoS,CHALL,MBA1,2)' },
+          { name: 'CSC 281.1 (CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 19' },
-          { name: 'Subject 20' },
+          { name: 'CSC482.1(CSC HALL 2)' },
+          { name: 'CHM130.1(FoS,CHALL,MBA1,2)' },
         ],
       ],
     Friday: [
         [
-          { name: 'CSC 394.1 (CSC HALL2)' },
-          { name: '' },
-          { name: '' },
+          { name: 'CSC 394.1 (CSC HALL2)' }
         ],
         [
-          { name: 'Subject 4' },
-          { name: 'Subject 5' },
-          { name: 'Subject 6' },
+          { name: 'STA 260.1 (MBA 2, MTH HALL)' },
+          { name: 'CSC486.1(CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 7' },
-          { name: 'Subject 8' },
+          { name: 'FSB101.1 (FoS,CHALL,MBA1)' },
+          { name: 'CSC 288.1 (MBA 2)' },
+          { name: 'CSC498.1 (CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 9' },
+          { name: 'PHY 101.1 (FoS,CHALL,MBA1,2)' },
+          { name: 'CSC498.1(CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 10' },
-          { name: 'Subject 11' },
+          { name: 'PHY 101.1 (FoS,CHALL,MBA1,2)' },
+          { name: 'CSC481.1(CSC HALL 2)' },
         ],
         [
-          { name: 'Subject 12' },
+          { name: 'JUMAT' },
         ],
         [
-          { name: 'Subject 13' },
-          { name: 'Subject 14' },
-          { name: 'Subject 15' },
+          { name: 'CSC 283.1 (MBA 1)' },
+          { name: 'CSC 382.1 (MBA 2)' },
         ],
         [
-          { name: 'Subject 16' },
-          { name: 'Subject 17' },
-          { name: 'Subject 18' },
+          { name: 'CSC 283.1 (MBA 1)' },
         ],
         [
-          { name: 'Subject 19' },
-          { name: 'Subject 20' },
+          { name: 'MTH 210.1(MTH, MBA 2)' },
+          { name: 'GES 400.1(MBS 22)' },
+        ],
+        [
+          { name: 'CSC180.1(CSC Hall 2)' },
         ],
       ],
     Saturday: [
